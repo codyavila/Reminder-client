da1b7e885e16740e6859b23b12da060685d9ac3b
sanderscody26 <sanderscody26@gmail.com>
2022-05-25 11:41:27 -0400

    Added auth functionality

--- app/app.js
+++ app/app.js
@@ -1,9 +1,16 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const authEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  // auth events
+  $('.logged-in').hide()
+  $('.sign-out').hide()
+  $('#change-password-form').hide()
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#sign-out').on('click', authEvents.onSignOut)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
 })
